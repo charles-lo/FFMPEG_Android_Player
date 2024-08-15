cb7f6ca83299fdadb7341c700fdb449b3e63af97
Charles Lo <clo@vewd.com>
2024-08-16 01:58:56 +0800

    integrate FFMPEG library

--- FFMPEGplayer/app/src/main/cpp/player.cpp
+++ FFMPEGplayer/app/src/main/cpp/player.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_charles_ffmpegplayer_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++ 001";
+    std::string hello = "Hello from C++ 002";
     return env->NewStringUTF(hello.c_str());
 }
